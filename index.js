2029b3d76a22df20295b607cbb8b50f9af476029
rodrieri000 <ericarodriguez7@outlook.com>
2020-02-25 19:33:48 -0600

    Update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use('/users', usersRouter)
 app.use('/auth', authRouter)
 
 app.get('/', (req, res) => {
-  res.send('Welcome to our server!')
+  res.send('welcome to our updated server')
 })
 
 app.listen(port, () => {
